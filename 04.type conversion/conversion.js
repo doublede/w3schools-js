7aeeec1328b7eaac1a1a037ffedcada8f77c36d6
德德 <double_de@163.com>
2017-04-12 00:14:12 +0800

    doublede

---
+++ 04.type conversion/conversion.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 德德 on 2017/4/12.
+ */
+console.log('123', typeof 123);
+console.log('"apple"', typeof 'apple');
+console.log('function(){}', typeof function () {
+});
+console.log('{}', typeof {});
+console.log('[1,2,3]', typeof [1, 2, 3]);
+console.log('new Date()', typeof new Date());
+console.log('NaN', typeof NaN);
+console.log('true', typeof true);
+console.log('fghj', typeof fghj);
+console.log('null', typeof null);
