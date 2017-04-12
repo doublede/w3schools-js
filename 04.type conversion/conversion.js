95b943249d3bf14aab5ded4f88e365e838a77248
德德 <double_de@163.com>
2017-04-12 23:35:51 +0800

    doublede

--- 04.type conversion/conversion.js
+++ 04.type conversion/conversion.js
@@ -12,3 +12,16 @@ console.log('NaN', typeof NaN);
 console.log('true', typeof true);
 console.log('fghj', typeof fghj);
 console.log('null', typeof null);
+
+//输出data的原型函数
+console.log((123), (123).constructor);
+console.log('"apple"', "apple".constructor);
+console.log('function(){}', function () {
+}.constructor);
+console.log('{}', {}.constructor);
+console.log('[1,2,3]', [1, 2, 3].constructor);
+console.log('new Date()', new Date().constructor);
+console.log('NaN', NaN.constructor);
+console.log('true', true.constructor);
+console.log('null', null.constructor);
+//console.log('asd',asd.constructor);         //报错
