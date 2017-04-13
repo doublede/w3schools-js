4a5c84f0845f0a001cf20d1a5ff0c394142a2fa8
德德 <double_de@163.com>
2017-04-14 00:15:35 +0800

    doublede

--- 04.type conversion/conversion.js
+++ 04.type conversion/conversion.js
@@ -23,5 +23,22 @@ console.log('[1,2,3]', [1, 2, 3].constructor);
 console.log('new Date()', new Date().constructor);
 console.log('NaN', NaN.constructor);
 console.log('true', true.constructor);
-console.log('null', null.constructor);
+//console.log('null', null.constructor);        //报错Cannot read property 'constructor' of null
 //console.log('asd',asd.constructor);         //报错
+
+//converting to string
+console.log(String(123));        //123
+console.log(String([1, 2, 3]));      //1,2,3
+console.log(String(new Date()));
+console.log(String(NaN));
+console.log(String(true));
+console.log(String(null));
+console.log(String(undefined));
+console.log(String(function () {
+}));
+//console.log(String(sadfd));       //报错sadfd is not defined
+console.log(String(123 + 34));
+console.log(String('123' + 34));
+console.log(String(123 + '34'));
+console.log(null.toString());          //toString类等于String(),但是这里会报错Cannot read property 'toString' of null
+console.log(undefined.toString());          // undefined也会报错，所以在用toString前要先做判断
