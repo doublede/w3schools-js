7c0ff423bc316d281994db2cf4c0b98434d51759
德德 <double_de@163.com>
2017-04-17 23:57:02 +0800

    doublede

---
+++ 05.bitwise/bitwise.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 德德 on 2017/4/17.
+ */
+//JS使用64位有符号数字
+console.log(1&1,30&30);    //按位与
+console.log(1|1,0|1,13|30);    //按位或
+console.log(~1,~25);      //按位非
+console.log(1^1);   //按位异或
+console.log(6<<1);      //左移 相当于*2
+console.log(6>>1);       //右移  相当于/2
+
