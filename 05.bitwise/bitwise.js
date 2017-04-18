77d9963617de3b3011b9fe3bd7520fbf227d7a60
德德 <double_de@163.com>
2017-04-18 18:28:22 +0800

    doublede

--- 05.bitwise/bitwise.js
+++ 05.bitwise/bitwise.js
@@ -9,3 +9,7 @@ console.log(1^1);   //按位异或
 console.log(6<<1);      //左移 相当于*2
 console.log(6>>1);       //右移  相当于/2
 
+console.log(-6<<1);     //除符号位外左移 相当于*2
+console.log(-6>>1);    //除符号位外右移
+console.log(6>>>1);       //右移（包括符号位）,最后一位被舍弃
+console.log(-6>>>1);       //包括符号位的右移,负值变正值
